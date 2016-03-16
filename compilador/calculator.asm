1ba2a90ad3cce9c131fd2524bf3f9666e87bc0eb
spanish inquisition <wtpc@gorgona>
2016-03-16 08:23:42 -0400

    de vuelta los archivos de compilacion

---
+++ compilador/calculator.asm
@@ -0,0 +1,65 @@
+	.file	"calculator.c"
+	.section	.rodata
+	.align 4
+.LC0:
+	.string	"I know how to add! 31 + 11 is %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	leal	4(%esp), %ecx
+	.cfi_def_cfa 1, 0
+	andl	$-16, %esp
+	pushl	-4(%ecx)
+	pushl	%ebp
+	.cfi_escape 0x10,0x5,0x2,0x75,0
+	movl	%esp, %ebp
+	pushl	%ecx
+	.cfi_escape 0xf,0x3,0x75,0x7c,0x6
+	subl	$20, %esp
+	subl	$8, %esp
+	pushl	$11
+	pushl	$31
+	call	add_numbers
+	addl	$16, %esp
+	movl	%eax, -12(%ebp)
+	subl	$8, %esp
+	pushl	-12(%ebp)
+	pushl	$.LC0
+	call	printf
+	addl	$16, %esp
+	movl	$0, %eax
+	movl	-4(%ebp), %ecx
+	.cfi_def_cfa 1, 0
+	leave
+	.cfi_restore 5
+	leal	-4(%ecx), %esp
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.globl	add_numbers
+	.type	add_numbers, @function
+add_numbers:
+.LFB1:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	movl	8(%ebp), %edx
+	movl	12(%ebp), %eax
+	addl	%edx, %eax
+	popl	%ebp
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	add_numbers, .-add_numbers
+	.ident	"GCC: (Debian 4.9.2-10) 4.9.2"
+	.section	.note.GNU-stack,"",@progbits
